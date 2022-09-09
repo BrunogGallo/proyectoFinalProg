4b821225304573a0db07e7b6288cb96f2943ec79
BrunogGallo <brunogiorgiogallo@mail.com>
2022-09-08 22:16:15 -0300

    termino funcionalidad de ver todas

--- app/src/Components/MovieCard/MovieCard.js
+++ app/src/Components/MovieCard/MovieCard.js
@@ -27,12 +27,12 @@ class MovieCard extends Component {
         console.log(this.props.movieData);
         return (
             <React.Fragment>
-                <article>
-                    <img src={`https://image.tmdb.org/t/p/w780/${this.props.movieData.poster_path}`} alt={this.props.movieData.title} />
+                <article className="movie-card">
                     <Link className="movie-content" to={`movie/id/${[this.props.movieData.id]}`}>
+                    <img src={`https://image.tmdb.org/t/p/w780/${this.props.movieData.poster_path}`} alt={this.props.movieData.title} />
                         <h2>{this.props.movieData.title}</h2>
-                    </Link>
                     <p>{this.state.description}</p>
+                    </Link>
 
                     {
                         this.state.description === ''
--- app/src/Screens/MovieDetail/MovieDetail.js
+++ app/src/Screens/MovieDetail/MovieDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 
+
 class MovieDetail extends Component {
 
     constructor(props) {
