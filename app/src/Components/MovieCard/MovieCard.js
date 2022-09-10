4268f1443eae30ddac2cb6b006c00d81fe04a3f2
BrunogGallo <brunogiorgiogallo@mail.com>
2022-09-10 20:21:57 -0300

    refresh en favoritos

--- app/src/Components/MovieCard/MovieCard.js
+++ app/src/Components/MovieCard/MovieCard.js
@@ -13,20 +13,20 @@ class MovieCard extends Component {
 
         }
     }
-    componentDidMount(){
+    componentDidMount() {
 
         let favoritos = [];
 
 
         let recuperoStorage = localStorage.getItem('favoritos'); // Esta posicion puede no estar.
 
-        if (recuperoStorage !== null){
+        if (recuperoStorage !== null) {
 
             let storageToArray = JSON.parse(recuperoStorage); //transforma texto en array.
 
             favoritos = storageToArray
 
-            if(favoritos.includes(this.props.movieData.id)){
+            if (favoritos.includes(this.props.movieData.id)) {
                 this.setState({
                     favsMessage: 'Quitar de favoritos'
                 })
@@ -35,44 +35,47 @@ class MovieCard extends Component {
 
     }
 
-    agregarYQuitarFavoritos(id){
+    agregarYQuitarFavoritos(id) {
         // vamos a guardar ids en un array dentro de localStorage.
         //console.log('Agregando y sacando favs');
-    
+
         let favoritos = [];
         let recuperoStorage = localStorage.getItem('favoritos'); // Esta posicion puede no estar.
 
-        if (recuperoStorage !== null){
+        if (recuperoStorage !== null) {
 
             let storageToArray = JSON.parse(recuperoStorage); //transforma texto en array.
 
             favoritos = storageToArray
         }
-        
 
 
-        if(favoritos.includes(id)){ // includes retorna true o false.
+
+        if (favoritos.includes(id)) { // includes retorna true o false.
             // Cambiar el texto del boton a Quitar de favoritos.
             // Sacar el id del array: usamos filter.
             favoritos = favoritos.filter(cadaIdDelArray => cadaIdDelArray !== id)
             this.setState({
                 favsMessage: 'Agregar a favoritos',
-            })
+            },
+            ()=> window.location.reload(false)
+            )
+            
         } else {
             favoritos.push(id);
             this.setState({
                 favsMessage: 'Quitar de favoritos',
             })
         }
-        
-        
+
+
 
         let favsToString = JSON.stringify(favoritos);
         localStorage.setItem('favoritos', favsToString)
 
         console.log(localStorage);
-    
-    
+
+
     }
     verMas() {
         this.setState({
@@ -87,27 +90,24 @@ class MovieCard extends Component {
     }
 
 
-    render () {
+    render() {
         return (
             <React.Fragment>
 
-            <article className="movie-card">
-                <Link className="movie-content" to={`movie/id/${this.props.movieData.id}`}>
-                    <img src={`https://image.tmdb.org/t/p/w780/${this.props.movieData.poster_path}`} alt={this.props.movieData.title} />
-                    <h2>{this.props.movieData.title}</h2>
-                    <p>{this.state.description}</p>
-                </Link>
-                        
+                <article className="movie-card">
+                    <Link className="movie-content" to={`movie/id/${this.props.movieData.id}`}>
+                        <img src={`https://image.tmdb.org/t/p/w780/${this.props.movieData.poster_path}`} alt={this.props.movieData.title} />
+                        <h2>{this.props.movieData.title}</h2>
+                        <p>{this.state.description}</p>
+                    </Link>
+                    {
+                        this.state.description === ''
+                            ? <p onClick={() => this.verMas()}>Mostrar mas</p>
+                            : <p onClick={() => this.verMenos()}>Mostrar menos</p>
 
-
-                        {
-                            this.state.description === ''
-                                ? <p onClick={() => this.verMas()}>Mostrar mas</p>
-                                : <p onClick={() => this.verMenos()}>Mostrar menos</p>
-
-                        }
-                <button onClick={()=>this.agregarYQuitarFavoritos(this.props.movieData.id)}>{this.state.favsMessage}</button>
-            </article>
+                    }
+                    <button onClick={() => this.agregarYQuitarFavoritos(this.props.movieData.id)}>{this.state.favsMessage}</button>
+                </article>
             </React.Fragment>
         )
     }
