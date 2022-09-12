4892c5a97b6ed87477c18971154e0eef6f914b73
Foncho81z <alfonsoagote@gmail.com>
2022-09-12 14:32:37 -0300

    Casi cerrando el navbar. Falta terminar de organizar donde va cada elemento

--- app/src/Components/Navbar/Navbar.js
+++ app/src/Components/Navbar/Navbar.js
@@ -23,21 +23,18 @@ class Navbar extends Component {
             <div className="navbar">
                 <img className="logo" src={'/img/logo.jpg'} alt='logo'/>
                 <ul className="links">
-                    <li><Link to="/"> Home </Link></li>
-                    <li><Link to="/popular"> Populares</Link></li>
-                    <li><Link to="/top-rated">Top Ranking</Link></li>
-                    <li><Link to="/favorites"> Mis Favoritas</Link></li>
-                    <li> 
-                    <form className="search-bar" onSubmit={e => this.evitarSubmit(e)}>
+                    <li><Link to="/"> HOME </Link></li>
+                    <li><Link to="/popular"> POPULARES</Link></li>
+                    <li><Link to="/top-rated">TOP RANKING</Link></li>
+                    <li><Link to="/favorites"> MIS FAVORITAS</Link></li>
+                </ul>                   
+                <form className="search-bar" onSubmit={e => this.evitarSubmit(e)}>
                         <input className="search-bar_input" type='text' onChange={e => this.cambiarDatos(e)} value={this.state.valor}></input>
                         <Link to={`/searchResults/${this.state.valor}`}>
                             <button className="search-bar_submit">            <i id="icono_navbar" class="fa-solid fa-magnifying-glass"></i>
                             </button>
                         </Link>
-                    </form>
-
-                    </li>
-                </ul>                   
+                </form>
             </div>
         )
     }
